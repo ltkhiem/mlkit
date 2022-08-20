8912bd27040959be19161b74a89dda526b2ebf22
Tu-Khiem Le <khiemletu@gmail.com>
2022-08-20 23:44:39 +0100

    :ambulance: [Fix] Group features for 'logo' split -- NaN value encountered in 'ridge' clf

--- classification/experiment.py
+++ classification/experiment.py
@@ -21,8 +21,8 @@ class Experiment():
             target,
             ignore_features = None,
             mlflow_uri = './',
+            experiment_exists_ok = False,
             data_splitter = 'sss',
-            stratify_features = None,
             stratify_splits = 2, 
             stratify_test_size = 0.2,
             group_features = None,
@@ -33,12 +33,14 @@ class Experiment():
             feature_selection = False,
             feature_selection_method = None,
             classifiers = 'all',
+            run_tags = None,
             random_state = 64
     ):
 
         mlflow.set_tracking_uri(mlflow_uri)
         mlflow.sklearn.autolog(log_post_training_metrics=False)
-        self._load_experiment(experiment_name)
+        self._load_experiment(experiment_name, experiment_exists_ok)
+        self.run_tags = run_tags
 
         self.data = data
         self.target = target
@@ -46,7 +48,7 @@ class Experiment():
 
 
         self.transformation = transformation
-        if self.transformation is not None:
+        if self.transformation:
             assert transformation_method, "Please specified transformation method"
             self.transformation_method = transformation_method
 
@@ -61,8 +63,6 @@ class Experiment():
 
         self.data_splitter = data_splitter
         if self.data_splitter == 'sss':
-            if stratify_features is None:
-                stratify_features = target
             self._splitter = StratifiedShuffleSplit(n_splits=stratify_splits, 
                                                         test_size=stratify_test_size,
                                                         random_state=random_state)
@@ -71,6 +71,7 @@ class Experiment():
                                              Leave One Group Out split."
             self._splitter = LeaveOneGroupOut()
             self.group_features = group_features
+            self.data_groups = data[group_features].values
 
 
 
@@ -78,15 +79,18 @@ class Experiment():
 
         self._gen_pipeline() 
 
-    def _load_experiment(self, experiment_name):
+    def _load_experiment(self, experiment_name, exp_exists_ok):
         expm = mlflow.get_experiment_by_name(experiment_name)
         if expm is not None:
-            confirm = input(f"Experiment {experiment_name} exists. Press Enter to \
-                    continue, type 'quit' to abort operation.")
-            if confirm == '': 
+            if exp_exists_ok:
                 self.exp_id = dict(expm)['experiment_id']
                 return
-            raise Exception("Experiment exists")
+            else:
+                confirm = input(f"Experiment {experiment_name} exists. Press Enter to continue, type 'quit' to abort operation.\n")
+                if confirm == '': 
+                    self.exp_id = dict(expm)['experiment_id']
+                    return
+                raise Exception("Experiment exists")
 
         self.exp_id = mlflow.create_experiment(experiment_name)
 
@@ -101,8 +105,8 @@ class Experiment():
     def _gen_splits(self, X, y): 
         if self.data_splitter == 'sss':
             return self._splitter.split(X, y)
-        elif data_splitter == 'logo':
-            return self._splitter.split(X, y, self.group_features)
+        elif self.data_splitter == 'logo':
+            return self._splitter.split(X, y, self.data_groups)
 
 
     def _gen_pipeline(self):
@@ -136,7 +140,13 @@ class Experiment():
             d = model.decision_function(X)
             y_proba = np.array([np.exp(d[x])/np.sum(np.exp(d[x]))
                 for x in range(d.shape[0])])
-        
+            if np.isnan(y_proba).any():
+                y_proba = np.nan_to_num(y_proba, nan=0, posinf=0)
+                _idx, = np.where(y_proba.sum(axis=1) != 1)
+                _size = y_proba[0].shape[0]
+                for i in _idx:
+                    y_proba[i] = np.full((1, _size), 1/_size)
+
 
         acc = accuracy_score(y_true, y_pred)
         auc_ovr = roc_auc_score(y_true, y_proba, multi_class='ovr')
@@ -168,7 +178,10 @@ class Experiment():
                 run_name=clf,
                 experiment_id=self.exp_id,
             ) as parent_run:
-                mlflow.log_param("parent", "yes")
+                mlflow.set_tags({'clf': clf})
+                if self.run_tags is not None:
+                    mlflow.set_tags(self.run_tags)
+
                 params = _all_clfs[clf]
                 self.pipe.set_params(**params) 
 
@@ -179,7 +192,6 @@ class Experiment():
                         experiment_id=self.exp_id,
                         nested=True,
                     ) as child_run:
-                        mlflow.log_param("child", "yes")
                         X_train, X_val = X[train_index], X[validate_index]
                         y_train, y_val = y[train_index], y[validate_index]
 
