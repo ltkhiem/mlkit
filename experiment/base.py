53b86c0da0c3ab7cc7b27c00ea7371f50eb16bbe
Tu-Khiem Le <khiemletu@gmail.com>
2022-08-28 12:55:49 +0100

    :hammer: [Dev] Restructure BaseExperiment class

--- experiment/base.py
+++ experiment/base.py
@@ -23,12 +23,38 @@ class BaseExperiment():
             run_tags = None,
             random_state = 64,
     ):
+        self._setup_mlflow(mlflow_ui,
+                experiment_name,
+                experiment_exists_ok,
+                run_tags)
+        self._setupdat(data,
+                target,
+                test_data,
+                ignore_features,
+                use_features)
+        self.random_state = random_state
+        self.use_cache = use_cache
 
+
+    def _set_up_mlflow(self, 
+            mlflow_uri, 
+            experiment_name, 
+            experiment_exists_ok,
+            run_tags
+    ):
         mlflow.set_tracking_uri(mlflow_uri)
         mlflow.sklearn.autolog(log_post_training_metrics=False)
         self.exp_id = mlflow_hp.load_experiment(experiment_name, experiment_exists_ok)
         self.run_tags = run_tags
 
+
+    def _setup_data(self,
+            data, 
+            target,
+            test_data,
+            ignore_features,
+            use_features,
+    ):
         self.data = data
         self.data.reset_index(drop=True, inplace=True)
         self.test_data = test_data
@@ -37,9 +63,6 @@ class BaseExperiment():
         self.target = target
         self.ignore_features = ignore_features
         self.use_features = use_features
-
-        self.random_state = random_state
-        self.use_cache = use_cache
 
 
     @mlflow_hp.check_active_run
